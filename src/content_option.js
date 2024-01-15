cccea01bf19c25e7f2061e6a946c80c29b66a420
dev-keshav <keshavkumar9431@gmail.com>
2024-01-16 01:44:51 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "KK";
+const meta = {
+    title: "Keshav Kumar",
+    description: "I’m Keshav Kumar _ Full stack devloper,currently working in a start-up",
+};
+
+const introdata = {
+    title: "I’m Keshav Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "I'm a full-stack web developer with background in React.Js, Next.Js Javascript, Firebase, Node.Js and Express.Js",
+    your_img_url: "https://i.pinimg.com/originals/51/da/1e/51da1e72bd270ee04d8704812d0a6ef1.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a full stack web developer having experiences in React.Js, Next.Js, Javascript, Firebase, Node.Js, Express.Js, MongoDb, HTML, CSS, UI Frameworks and API Integration. ",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "India",
+        date: "2023",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "React.Js",
+        value: 90,
+    },
+    {
+        name: "Next.Js",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Node.Js",
+        value: 60,
+    },
+    {
+        name: "Express.Js",
+        value: 85,
+    },
+];
+
+const services = [
+    // {
+    //     title: "UI & UX Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    {
+        title: "Web Apps",
+        description: "I'm developing and keeping up with the entire stack of web apps, including the front end, back end, and database administration. I am knowledgeable about programming languages, frameworks, and tools. I've worked with React, a programme used to build dynamic websites.",
+    },
+    // {
+    //     title: "Wordpress Website",
+    //     description: "Both front-end and back-end development, along with the use of plugins and themes, are under my purview. My objective is to develop appealing, user-friendly websites that adhere to customer requirements.",
+    // },
+];
+
+const dataportfolio = [{
+        img: "https://3.bp.blogspot.com/-JrWn0KbB6RQ/WDImlJbV-9I/AAAAAAAADeQ/QcCxqtuDYp0FbTMX0V05bhG9CfdljDb4QCLcB/s1600/863370_e352_5.jpg",
+        desctiption: "Search github repo Web app",
+        link: "https://github.com/dev-keshav/git-search-repo",
+    },
+    {
+        img: "https://cdn.dribbble.com/users/77531/screenshots/16645267/media/78cd3639944059ae0344ccd70ba71c30.png",
+        desctiption: "Restfull API which has the data for different exercises.",
+        link: "https://github.com/dev-keshav/workoutApi",
+    },
+    {
+        img: "https://cdn.dribbble.com/users/1696728/screenshots/19197570/media/c5158e5a40cec1040618ef57b9eb8478.png",
+        desctiption: "Social media web applicaion using react.",
+        link: "https://github.com/dev-keshav/your-social-media",
+    },
+    {
+        img: "https://electride.in/images/Navigation-bro.png",
+        desctiption: "Landing page using HTML, CSS & Javascript.",
+        link: "https://electride.in/",
+    },
+    {
+        img: "https://groobe-landing.netlify.app/images/bride-7638298%202.jpg",
+        desctiption: "Landing page",
+        link: "https://groobe-landing.netlify.app/",
+    },
+
+    {
+        img: "https://i.pinimg.com/736x/cf/9c/5b/cf9c5be74853a72ce3aa4590e9b91300.jpg",
+        desctiption: "Audio to text conversion.",
+        link: "https://github.com/dev-keshav/audio-to-text-py",
+    },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     desctiption: "Speech to text virtual notes maker using javascript and api.",
+    //     link: "https://live-voice2text.netlify.app/",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "keshavkumar9431@gmail.com",
+    // YOUR_FONE: "After receiving your message",
+    // description: "Anytime you can message me, I will undoubtedly respond. Please send your question if you have any questions about technologies or any problems with software projects. ",
+    description: "Send your query ->",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/dev-keshav",
+    facebook: "https://www.facebook.com/profile.php?id=100029481072194",
+    linkedin: "https://www.linkedin.com/in/helo-keshav/",
+    twitter: "https://twitter.com/helokeshav",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
